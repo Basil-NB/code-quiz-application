5eff0df00ade2cd83c1a2889d3f4b7bd6f540c59
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2023-02-21 14:24:32 -0500

    added css styling aswel as basic js funtions

---
+++ assets/.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById("start-button")
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
