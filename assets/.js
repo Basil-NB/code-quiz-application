a00e8d351d1f54cbcf0620276956c7d07f0e0788
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2023-02-21 23:44:52 -0500

    added some js functionality

--- assets/.js
+++ assets/.js
@@ -1,9 +1,24 @@
 var startButton = document.getElementById("start-button")
+var questionContainetEl = document.getElementById('question-container')
+var quizIntro = document.getElementById('quiz-start')
+
+var questions = [
+    {
+        question: 'what is an array',
+        answer: [
+            {}
+        ]
+    }
+]
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
-
+console.log('started')
+startButton.classList.add('hide')
+questionContainetEl.classList.remove('hide')
+quizIntro.classList.add('hide')
+setNextQuestion()
 }
 
 function setNextQuestion(){
